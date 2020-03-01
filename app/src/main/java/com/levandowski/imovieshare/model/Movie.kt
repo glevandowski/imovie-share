3388e3972b304dc80a6be12164034458eb9be583
glevandowski <gustavolevandowski@gmail.com>
2020-03-01 16:02:43 -0300

    Refactor and adjust layouts

--- app/src/main/java/com/levandowski/imovieshare/model/Movie.kt
+++ app/src/main/java/com/levandowski/imovieshare/model/Movie.kt
@@ -1,18 +1,48 @@
 package com.levandowski.imovieshare.model
 
+import androidx.recyclerview.widget.DiffUtil
+import com.google.gson.annotations.SerializedName
+
 data class Movie(
+    @SerializedName("popularity")
     val popularity: Double? = null,
+    @SerializedName("vote_count")
     val voteCount: Int? = null,
+    @SerializedName("video")
     val video: Boolean? = null,
+    @SerializedName("poster_path")
     val posterPath: String? = null,
+    @SerializedName("id")
     val id: Int? = null,
+    @SerializedName("adult")
     val adult: Boolean? = null,
+    @SerializedName("backdrop_path")
     val backdropPath: String? = null,
+    @SerializedName("original_language")
     val originalLanguage: String? = null,
+    @SerializedName("original_title")
     val originalTitle: String? = null,
+    @SerializedName("genre_ids")
     val genreIds: List<Int>? = null,
+    @SerializedName("title")
     val title: String? = null,
+    @SerializedName("vote_average")
     val voteAverage: Double? = null,
+    @SerializedName("overview")
     val overview: String? = null,
+    @SerializedName("release_date")
     val releaseDate: String? = null
-)
+) {
+
+    companion object {
+        val DIFF_CALLBACK: DiffUtil.ItemCallback<Movie> = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
--- app/src/main/java/com/levandowski/imovieshare/movies/MoviesViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.levandowski.imovieshare.movies
-
-import androidx.lifecycle.ViewModel
-
-class MoviesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
